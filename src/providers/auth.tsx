f500c3097b888238c9a422be23011bafda176f29
kristoffer <kunkristoffer@gmail.com>
2025-01-25 18:46:03 +0100

    added auth provider

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -3,6 +3,7 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "@/lib/firebase/auth";
 import { AuthStore } from "@/types/auth.types";
+import { useRouter } from "next/navigation";
 
 // Init value for context
 const AuthContext = createContext<AuthStore>({ user: null, signOut });
@@ -15,13 +16,17 @@ export function useAuthContext() {
 // Auth provider component wrapper
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<AuthStore["user"]>(null);
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged((res) => {
+      if (!res?.displayName) {
+        router.push("/");
+      }
       setUser(res);
     });
     return () => unsubscribe();
-  }, []);
+  }, [router]);
 
   return <AuthContext.Provider value={{ user, signOut }}>{children}</AuthContext.Provider>;
 }
