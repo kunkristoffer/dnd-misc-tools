3a9c2e0aaf0f9167e3606f4a31f011587b8a6829
kristoffer <kunkristoffer@gmail.com>
2025-01-26 00:56:34 +0100

    added page to view all items

---
+++ src/app/items/all/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { getAllItems } from "@/lib/firebase/firestore/items";
+import { DnDItem } from "@/types/dnd/items.types";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [items, setItems] = useState<DnDItem[]>();
+  useEffect(() => {
+    async function update() {
+      const data = await getAllItems({});
+      setItems(data.data);
+    }
+    update();
+  }, []);
+  return (
+    <main>
+      <div className="flex flex-col gap-2">
+        {items?.map((item) => (
+          <span key={item.id} className="flex gap-2">
+            <p>{item.name}</p>
+            <Link href={`/items/id/${item.id}`} className="bg-secondary textp">
+              open
+            </Link>
+          </span>
+        ))}
+      </div>
+    </main>
+  );
+}
