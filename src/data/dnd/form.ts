04f29140aad9dbece143ccf42d7ab4ecc5696532
kristoffer <kunkristoffer@gmail.com>
2025-01-25 22:02:34 +0100

    added helper vars for creating forms

---
+++ src/data/dnd/form.ts
@@ -0,0 +1,43 @@
+import { DnDItem, DnDItemTypes, iDnDsubtypesValues } from "@/types/dnd/items.types";
+
+// prettier-ignore
+export const formAvailableRarity = [ "mundane", "common", "uncommon", "rare", "very-rare", "legendary", "artifact", ] as const;
+
+// prettier-ignore
+export const formAvailableTypes = [ "weapon", "armor", "ammo", "prefix", "suffix", "trinket", "wearables", "companion", ] as const;
+
+// prettier-ignore
+export function formGetSubTypes(type: DnDItemTypes) {
+  if (!type) return []
+  switch (true) {
+    case type.includes("fix"):
+      return ["weapon", "armor", "ammo"];
+    case type === "armor":
+      return ["light", "medium", "heavy", "shield"];
+    case type === "weapon":
+      return ["simple melee", "simple ranged", "martial melee", "martial ranged"];
+    case type === "wearables":
+      return [ "armband", "belt", "boots", "cloak", "earring", "facegear", "gloves", "headgear", "necklace", "pauldron", "ring", "tattoo", ];
+    case type === "companion":
+      return ["humanoid", "dragonborn", "robotic"];
+    default:
+      return [];
+  }
+}
+
+export function formGetBases(subtypes: iDnDsubtypesValues[]) {
+  // todo subtypes
+}
+
+export const formItemInit: DnDItem = {
+  name: "",
+  rarity: "",
+  type: "",
+  price: 0,
+  source: "",
+  subType: [],
+  attuned: false,
+  bases: [],
+  image: "",
+  description: "",
+};
