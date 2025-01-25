7daaa6f36510cd8a156c25eae054e767c3d352e5
kristoffer <kunkristoffer@gmail.com>
2025-01-25 18:46:35 +0100

    added user menu to navbar

--- src/components/navigation/navBar.tsx
+++ src/components/navigation/navBar.tsx
@@ -1,12 +1,19 @@
+"use client";
+
+import { useAuthContext } from "@/providers/auth";
 import { NavBarLink } from "../ui/links/navBar";
 import menuItem from "@/data/navigation/navBarLinks.json";
 
-export async function HeaderNavBar() {
-  return (
-    <nav className="flex items-center">
-      {menuItem.map((item, index) => (
-        <NavBarLink key={`${item.label}${index}`} {...item} />
-      ))}
-    </nav>
-  );
+export function HeaderNavBar() {
+  const { user } = useAuthContext();
+  if (user)
+    return (
+      <nav className="flex items-center">
+        {menuItem.map((item, index) => (
+          <NavBarLink key={`${item.label}${index}`} {...item} />
+        ))}
+      </nav>
+    );
+
+  return null;
 }
