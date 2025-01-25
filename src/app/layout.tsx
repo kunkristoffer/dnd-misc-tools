228c7476d0e93c0a1ff4f5c44bdcdb37163a91d6
kristoffer <kunkristoffer@gmail.com>
2025-01-25 17:46:38 +0100

    adding auth provider to base layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Footer } from "@/components/elements/footer";
 import { Header } from "@/components/elements/header";
 import "@/assets/styles/global.css";
+import { AuthProvider } from "@/providers/auth";
 
 export const metadata: Metadata = {
   title: "D&D Tools",
@@ -14,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-background text-primary">
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className="bg-background text-primary">
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
