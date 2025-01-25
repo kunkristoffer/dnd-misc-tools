75584e7f5127f3bf7753f626da11d32c9e040545
kristoffer <kunkristoffer@gmail.com>
2025-01-25 16:23:48 +0100

    adding prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  useTabs: false,
+  singleAttributePerLine: false
+};
+
+export default config;
