aa3f27a84f69be08b05d39d3dbec0e6ecca09a49
kristoffer <kunkristoffer@gmail.com>
2025-01-26 22:34:33 +0100

    added select with single or multiple choice feature

--- src/app/items/edit/[itemId]/page.tsx
+++ src/app/items/edit/[itemId]/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { ItemPreview } from "@/components/ui/cards/itemPreview";
-import { formItemInit } from "@/data/dnd/form";
+import { InputCheckbox } from "@/components/ui/input/checkbox";
+import { InputNumber } from "@/components/ui/input/number";
+import { InputSelect } from "@/components/ui/input/select";
+import { InputText } from "@/components/ui/input/text";
+import { InputTextarea } from "@/components/ui/input/textarea";
+import { formAvailableRarity, formAvailableTypes, formItemInit } from "@/data/dnd/form";
 import { deleteItem, editItem, getItemById } from "@/lib/firebase/firestore/items";
 import { DnDItem, DnDItemRarity, DnDItemTypes } from "@/types/dnd/items.types";
 import { usePathname, useRouter } from "next/navigation";
@@ -11,7 +16,7 @@ export default function Page() {
   const [item, setItem] = useState<DnDItem>(formItemInit);
   const [deleteConfirm, setDeleteConfirm] = useState(false);
   const uid = usePathname().split("/").pop();
-  const router = useRouter()
+  const router = useRouter();
 
   async function handleDelete() {
     if (!uid || uid.length !== 20) return;
@@ -21,9 +26,9 @@ export default function Page() {
     if (deleteConfirm) {
       const res = await deleteItem(uid);
       if (res.code === 200) {
-        setDeleteConfirm(false)
-        setItem(formItemInit)
-        router.push("/items/create")
+        setDeleteConfirm(false);
+        setItem(formItemInit);
+        router.push("/items/create");
       }
     }
 
@@ -50,129 +55,112 @@ export default function Page() {
   return (
     <main>
       <div className="flex justify-between gap-12">
-        <div className="flex flex-col gap-2">
-          <h1>create item</h1>
-          <input
-            type="text"
-            name="name"
-            placeholder="name"
-            value={item.name}
-            onChange={(e) => setItem((prev) => ({ ...prev, name: e.target.value }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="number"
-            name="price"
-            placeholder="price"
-            value={item.price === 0 ? "" : item.price}
-            onChange={(e) => setItem((prev) => ({ ...prev, price: Number(e.target.value) }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="text"
-            name="rarity"
-            placeholder="rarity"
-            value={item.rarity}
-            onChange={(e) => setItem((prev) => ({ ...prev, rarity: e.target.value as DnDItemRarity }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="text"
-            name="type"
-            placeholder="type"
-            value={item.type}
-            onChange={(e) => setItem((prev) => ({ ...prev, type: e.target.value as DnDItemTypes }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="text"
-            name="subType"
-            placeholder="subType"
-            value={item.subType}
-            onChange={(e) => setItem((prev) => ({ ...prev, subType: [e.target.value] }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <span className=" bg-panel py-1 px-2">
-            <input
-              type="checkbox"
-              name="attuned"
-              placeholder="attuned"
-              value={item.attuned ? "on" : ""}
-              onChange={(e) => setItem((prev) => ({ ...prev, attuned: (e.target as HTMLInputElement).checked }))}
-              className="mr-2"
+        <div className="p-2 rounded-md bg-foreground">
+          <div className="flex flex-col gap-2 w-48">
+            <InputText
+              name="name"
+              label="Item name"
+              value={item.name}
+              onChange={(e) => setItem((prev) => ({ ...prev, name: e.target.value }))}
             />
-            requires attunement
-          </span>
-          <span className=" bg-panel py-1 px-2">
-            <input
-              type="checkbox"
-              name="homebrew"
-              placeholder="homebrew"
-              value={item.homebrew ? "on" : ""}
-              onChange={(e) => setItem((prev) => ({ ...prev, homebrew: (e.target as HTMLInputElement).checked }))}
-              className="mr-2"
+            <InputSelect
+              name="rarity"
+              label="Item rarity"
+              value={item.rarity}
+              options={formAvailableRarity}
+              onChange={(value) => setItem((prev) => ({ ...prev, rarity: value as DnDItemRarity }))}
             />
-            homebrew item
-          </span>
-          <input
-            type="text"
-            name="description"
-            placeholder="description"
-            value={item.description}
-            onChange={(e) => setItem((prev) => ({ ...prev, description: e.target.value }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="text"
-            name="source"
-            placeholder="source"
-            value={item.source}
-            onChange={(e) => setItem((prev) => ({ ...prev, source: e.target.value }))}
-            className="bg-panel py-1 px-2 rounded"
-          />
-          <input
-            type="text"
-            name="image"
-            placeholder="image (tmp disabled)"
-            disabled
-            value={item.image}
-            onChange={(e) => setItem((prev) => ({ ...prev, image: e.target.value }))}
-            className="bg-foreground py-1 px-2 rounded"
-          />
-          <span>
-            <button type="button" onClick={handleDelete} className="bg-red-500 p-2">
-              delete
-            </button>
-            <button type="button" onClick={handleSave} className="bg-green-500 p-2">
-              save
-            </button>
-          </span>
-          <span>
-            {deleteConfirm && (
-              <div className="z-50 fixed inset-0 top-10 backdrop-blur-sm backdrop-brightness-50 flex justify-center items-center">
-                <div className="flex flex-col items-center p-10 gap-4 rounded-md bg-panel">
-                  <h1>Youre about to delete this item, are you sure?</h1>
-                  <h2>There is no going back 😵</h2>
-                  <span className="flex justify-around w-full">
-                    <button
-                      type="button"
-                      className="bg-blue-500 px-4 py-2 rounded-md hover:scale-105 duration-200"
-                      onClick={() => setDeleteConfirm(false)}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="button"
-                      className="bg-red-500 px-4 py-2 rounded-md hover:scale-105 duration-200"
-                      onClick={handleDelete}
-                    >
-                      Delete
-                    </button>
-                  </span>
+            <InputSelect
+              name="type"
+              label="Item type"
+              value={item.type}
+              options={formAvailableTypes}
+              onChange={(value) => setItem((prev) => ({ ...prev, type: value as DnDItemTypes }))}
+            />
+            <InputSelect
+              name="subType"
+              label="Item subtype"
+              value={item.subType}
+              options={formAvailableTypes}
+              multiple
+              onChange={(value) => setItem((prev) => ({ ...prev, subType: value as string[] }))}
+            />
+            <span className="flex gap-2 w-full">
+              <InputCheckbox
+                name="attuned"
+                label="⌛"
+                title="Item requires attunement"
+                checked={item.attuned}
+                onChange={(e) => setItem((prev) => ({ ...prev, attuned: (e.target as HTMLInputElement).checked }))}
+              />
+              <InputCheckbox
+                name="homebrew"
+                label="🏠"
+                title="Item is a homebrew"
+                checked={item.homebrew}
+                onChange={(e) => setItem((prev) => ({ ...prev, homebrew: (e.target as HTMLInputElement).checked }))}
+              />
+            </span>
+            <InputNumber
+              name="name"
+              label="Item price"
+              value={item.price}
+              onChange={(e) => setItem((prev) => ({ ...prev, price: Number(e.target.value) }))}
+            />
+            <InputText
+              name="source"
+              label="Item source"
+              value={item.source ?? ""}
+              onChange={(e) => setItem((prev) => ({ ...prev, source: e.target.value }))}
+            />
+            <InputText
+              name="image"
+              label="Item image"
+              disabled
+              value={item.image ?? ""}
+              onChange={(e) => setItem((prev) => ({ ...prev, image: e.target.value }))}
+            />
+            <InputTextarea
+              name="description"
+              label="Item description"
+              value={item.description ?? ""}
+              onChange={(e) => setItem((prev) => ({ ...prev, description: e.target.value }))}
+            />
+            <span>
+              <button type="button" onClick={handleDelete} className="bg-red-500 p-2">
+                delete
+              </button>
+              <button type="button" onClick={handleSave} className="bg-green-500 p-2">
+                save
+              </button>
+            </span>
+            <span>
+              {deleteConfirm && (
+                <div className="z-50 fixed inset-0 top-10 backdrop-blur-sm backdrop-brightness-50 flex justify-center items-center">
+                  <div className="flex flex-col items-center p-10 gap-4 rounded-md bg-panel">
+                    <h1>Youre about to delete this item, are you sure?</h1>
+                    <h2>There is no going back 😵</h2>
+                    <span className="flex justify-around w-full">
+                      <button
+                        type="button"
+                        className="bg-blue-500 px-4 py-2 rounded-md hover:scale-105 duration-200"
+                        onClick={() => setDeleteConfirm(false)}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="bg-red-500 px-4 py-2 rounded-md hover:scale-105 duration-200"
+                        onClick={handleDelete}
+                      >
+                        Delete
+                      </button>
+                    </span>
+                  </div>
                 </div>
-              </div>
-            )}
-          </span>
+              )}
+            </span>
+          </div>
         </div>
         <div className="flex flex-col gap-2">
           <h1>item preview</h1>
